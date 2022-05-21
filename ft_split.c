a223672729b6ee602770abfadb7ff0b8c98dd9f6
sirtiffany <75033448+sirtiffany@users.noreply.github.com>
2022-05-21 15:57:56 +0200

    improved with ft_substr || added gitignore file

--- ft_split.c
+++ ft_split.c
@@ -6,33 +6,11 @@
 /*   By: marmoral <marmoral@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 14:48:39 by marmoral          #+#    #+#             */
-/*   Updated: 2022/05/21 13:12:26 by marmoral         ###   ########.fr       */
+/*   Updated: 2022/05/21 15:51:08 by marmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_strdup_mod(const char *s1, size_t size, size_t i)
-{
-	char	*s1_cpy;
-	size_t	x;
-
-	x = 0;
-	if (!s1)
-		return (NULL);
-	s1_cpy = malloc(size + 1);
-	if (!s1_cpy)
-		return (NULL);
-	while (size)
-	{
-		s1_cpy[x] = s1[i];
-		i++;
-		x++;
-		size--;
-	}
-	s1_cpy[x] = 0;
-	return (s1_cpy);
-}
 
 static	void	save(char **split, const char *s, char c)
 {
@@ -49,7 +27,7 @@ static	void	save(char **split, const char *s, char c)
 			count++;
 		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
 		{
-			split[x] = ft_strdup_mod(s, count, (i - (count - 1)));
+			split[x] = ft_substr(s, (i - (count - 1)), count);
 			x++;
 			count = 0;
 		}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,38 +6,11 @@
 /*   By: marmoral <marmoral@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 10:53:49 by marmoral          #+#    #+#             */
-/*   Updated: 2022/05/21 14:17:48 by marmoral         ###   ########.fr       */
+/*   Updated: 2022/05/21 15:46:39 by marmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static	char	*ft_strdup_mod(const char *s1, int size, int i)
-{
-	char	*s1_cpy;
-	size_t	x;
-
-	x = 0;
-	if (size == 0)
-		return (ft_strdup(&s1[i]));
-	if (size < 0)
-		return (ft_strdup(""));
-	if (!s1)
-		return (NULL);
-	size++;
-	s1_cpy = malloc(size + 1);
-	if (!s1_cpy)
-		return (NULL);
-	while (size)
-	{
-		s1_cpy[x] = s1[i];
-		i++;
-		x++;
-		size--;
-	}
-	s1_cpy[x] = 0;
-	return (s1_cpy);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -48,7 +21,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	x = 0;
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
 	while (set[x])
 	{
 		if (s1[i] == set[x])
@@ -56,13 +29,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 			i++;
 			x = -1;
 		}
-		else if (s1[end] == set[x] && end > i)
+		else if (s1[end - 1] == set[x] && end > i)
 		{
 			end--;
 			x = -1;
 		}
 		x++;
 	}
-	s1_trim = ft_strdup_mod(s1, (end - i), i);
+	s1_trim = ft_substr(s1, i, (end - i));
 	return (s1_trim);
 }
